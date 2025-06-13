d92260fb722f0eead24d8b4abe5466460f6e3fb0
RAGHUL M <69421772+raghulvj01@users.noreply.github.com>
2025-06-13 13:18:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,23 @@
-variable "region" {
+variable "aws_region" {
   description = "AWS region"
+  type        = string
   default     = "ap-southeast-1"
 }
 
-variable "key_name" {
-  description = "The name of the SSH key pair in AWS"
+variable "instance_type" {
+  description = "EC2 instance type"
   type        = string
+  default     = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name for the EC2 key pair"
+  type        = string
+  default     = "my-singapore-key"
+}
+
+variable "public_key_file" {
+  description = "Relative path to the public key file"
+  type        = string
+  default     = "id_rsa.pub"
 }
